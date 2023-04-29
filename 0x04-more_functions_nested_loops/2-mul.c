0a1e69a1ac2c76cf356a7e94ec7a4243b7424b31
missmeefa <emivuvor@yahoo.com>
2023-04-28 22:28:31 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - multiple two integers (0 through 9).
+ * @x: x - Variable
+ * @y: y - variable
+ * Return: Always 0.
+ */
+int mul(int x, int y)
+{
+	int mul;
+
+	mul = x * y;
+
+	return (mul);
+}
